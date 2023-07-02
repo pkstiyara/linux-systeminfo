fb427a191630a59611fc24787562f89f1f918e26
Pankaj Singh <pkstiyara@gmail.com>
2023-07-02 09:54:56 +0530

    Update systeminfo.sh

--- systeminfo.sh
+++ systeminfo.sh
@@ -9,7 +9,14 @@ get_system_info() {
   echo "Operating System: $(cat /etc/*release | grep -oP '(?<=PRETTY_NAME=").*?(?=")')"
   echo "CPU: $(grep 'model name' /proc/cpuinfo | uniq | awk -F ':' '{print $2}' | xargs)"
   echo "Memory: $(grep 'MemTotal' /proc/meminfo | awk '{print $2/1024/1024 " GB"}')"
-  echo "Disk Usage: $(df -h / | awk 'NR==2{print $5}')"
+  
+  disk_usage=$(df -h / | awk 'NR==2{print $5}')
+  total_disk=$(df -h / | awk 'NR==2{print $2}')
+  used_disk=$(df -h / | awk 'NR==2{print $3}')
+  available_disk=$(df -h / | awk 'NR==2{print $4}')
+  
+  echo "Disk Usage: $disk_usage ($used_disk used, $available_disk available out of $total_disk)"
+  
   echo "Uptime: $(uptime -p)"
   echo "Users Logged In: $(who | wc -l)"
   echo "Processes Running: $(ps -e --no-header | wc -l)"
